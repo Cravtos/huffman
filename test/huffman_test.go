562a93365a29101675535aeb8e536bf39af4a6a9
Nikita Kryukov <cravtos92@gmail.com>
2021-07-06 13:57:59 +0700

    Don't call FailNow() in subtests

--- test/huffman_test.go
+++ test/huffman_test.go
@@ -21,20 +21,26 @@ func TestHuffman(t *testing.T) {
 		file := file
 		t.Run(file.Name(), func(t *testing.T) {
 			t.Parallel()
-			encodeAndDecode(t, "./testdata/"+file.Name())
+			equal, err := cmpEncodeAndDecode(t, "./testdata/"+file.Name())
+			if err != nil {
+				t.Errorf("got error while encoding or decoding testdata: %v\n", err)
+			}
+			if equal != true {
+				t.Error("original and decoded files are not equal")
+			}
 		})
 	}
 }
 
-func encodeAndDecode(t *testing.T, file string) {
+// cmpEncodeAndDecode encodes and decodes files, and compares original to decoded one.
+func cmpEncodeAndDecode(t *testing.T, file string) (equal bool, err error) {
 	// Create temp directory for resulting files
 	tmp := t.TempDir()
 
 	origName := file
 	origFile, err := os.Open(origName)
 	if err != nil {
-		t.Errorf("got error while opening test file %s: %v\n", origName, err)
-		t.FailNow()
+		return false, err
 	}
 	defer origFile.Close()
 
@@ -42,61 +48,50 @@ func encodeAndDecode(t *testing.T, file string) {
 	encName := tmp + ".encoded"
 	encFile, err := os.Create(encName)
 	if err != nil {
-		t.Errorf("got error while opening encoded test file %s: %v\n", encName, err)
-		t.FailNow()
+		return false, err
 	}
 	defer encFile.Close()
 	defer os.Remove(encName)
 
 	err = huffman.Encode(origFile, encFile)
 	if err != nil {
-		t.Errorf("got error while encoding test file %s: %v\n", origName, err)
-		t.FailNow()
+		return false, err
 	}
 
 	// Seek to beginning (since it will be used again to be decoded)
 	_, err = encFile.Seek(0, 0)
 	if err != nil {
-		t.Errorf("got error while seeking test file %s: %v\n", encName, err)
-		t.FailNow()
+		return false, err
 	}
 
 	// Create file for decoded data
 	decName := tmp + ".decoded"
 	decFile, err := os.Create(decName)
 	if err != nil {
-		t.Errorf("got error while opening decoded test file %s: %v\n", decName, err)
-		t.FailNow()
+		return false, err
 	}
 	defer decFile.Close()
 	defer os.Remove(decName)
 
 	err = huffman.Decode(encFile, decFile)
 	if err != nil {
-		t.Errorf("got error while decoding test file %s: %v\n", encName, err)
-		t.FailNow()
+		return false, err
 	}
 
 	// Seek files to beginning for comparing
 	_, err = origFile.Seek(0, 0)
 	if err != nil {
-		t.Errorf("got error while seeking test file %s: %v\n", origName, err)
-		t.FailNow()
+		return false, err
 	}
 
 	_, err = decFile.Seek(0, 0)
 	if err != nil {
-		t.Errorf("got error while seeking test file %s: %v\n", decName, err)
-		t.FailNow()
+		return false, err
 	}
 
-	equal, err := helpers.CompareFiles(origFile, decFile)
+	equal, err = helpers.CompareFiles(origFile, decFile)
 	if err != nil {
-		t.Errorf("got error while comparing test files %s and %s: %v\n", origName, decName, err)
-		t.FailNow()
+		return false, err
 	}
-
-	if equal == false {
-		t.Errorf("original and decoded files are not equal: %s and %s", origName, decName)
-	}
+	return equal, nil
 }
